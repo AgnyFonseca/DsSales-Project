cfbae519580d339c00bde0e5f27cbca8b774649b
Agny <agnyfonseca@outlook.com>
2022-03-25 11:57:02 +0100

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import './assets/styles/custom.scss';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div>
-      <h1>DsSales</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar bg-light main-nav">
+            <div className="nav-logo-text">
+                <h4>BDS vendas</h4>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
