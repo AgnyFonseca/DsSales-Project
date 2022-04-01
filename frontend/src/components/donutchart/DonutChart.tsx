1d90b664b76e7c70243d3b0815e104e7d873f445
Agny <agnyfonseca@outlook.com>
2022-04-01 08:39:14 +0200

    Chart - Sales by genre and sum

--- frontend/src/components/donutchart/DonutChart.tsx
+++ frontend/src/components/donutchart/DonutChart.tsx
@@ -1,42 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
+import { SalesByGender } from '../../types/types';
+import { makeRequest } from '../../utils/requests';
 
 type ChartData = {
     labels: string[];
     series: number[];
 }
 
-type Props = {
-    labels?: string[];
-    name: string;
-    series?: number[];
-  };
-
 const DonutChart = () => {
-    // const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-    // useEffect(() => {
-    //     axios.get(`${BASE_URL}/sales/amount-by-seller`)
-    //         .then(response => {
-    //             const data = response.data as SaleSum[];
-    //             const myLabels = data.map(x => x.sellerName);
-    //             const mySeries = data.map(x => x.sum);
+    
 
-    //             setChartData({ labels: myLabels, series: mySeries });
-    //         });
-    // }, []);
+    useEffect(() => {
+        makeRequest
+            .get<SalesByGender[]>('/sales/by-gender')
+            .then((response) => {
+                const data = response.data as SalesByGender[];
+                const myLabels = data.map(x => x.gender);
+                const mySeries = data.map(x => x.sum);
 
-   
-    // const options = {
-    //     legend: {
-    //         show: true
-    //     }
-    // }
-
-    const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }
+                setChartData({ labels: myLabels, series: mySeries });
+            })
+    }, []);
 
     const options = {
         legend: {
@@ -46,8 +33,8 @@ const DonutChart = () => {
 
     return (
         <Chart 
-            options={{...options, labels: mockData.labels}}
-            series={mockData.series}
+            options={{...options, labels: chartData.labels}}
+            series={chartData.series}
             type="donut"
             height="400"
             width="400"
